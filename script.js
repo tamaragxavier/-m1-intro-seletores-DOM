d9a385ffb21e30f7b1021e927d70eba6476a41c2
Tamara Xavier <tamaragiliski96@gmail.com>
2025-01-08 21:35:04 -0300

    Prática da aula Seletore DOM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+//const div1 = document.getElementById("box_1");
 
+//div1.style.backgroundColor = "black";
+
+const todasDivsBox = document.querySelectorAll(".box");
+
+for ( let i = 0; i < todasDivsBox.length; i++){
+    todasDivsBox[i].style.backgroundColor = "red";
+}
